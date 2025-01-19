f9abe4854217b4e0bf9bd38c06df2724b392f927
NITSC <138109564+nitsc@users.noreply.github.com>
2025-01-19 16:00:45 +0800

    新增加密方法，符合中国标准

--- cilent/cilent.py
+++ cilent/cilent.py
@@ -4,24 +4,27 @@ import threading
 from datetime import datetime
 import sys
 sys.path.append("/crypt.py")
-from crypt import EcdhAesCrypt
+from crypt import EcdhAesCrypt, Curve25519Sm4
 from cryptography.hazmat.primitives import serialization
-import urllib.request
 
 
 
-def send_message(client_socket, shared_key):
+def send_message(client_socket, ae_shared_key, cs_shared_key):
+    cs = Curve25519Sm4()
     while True:
         message = input("客户端: ")
         if message.lower() == 'exit':
             break
-        encrypted_message = EcdhAesCrypt.encrypt_data(shared_key, message)
-        client_socket.send(encrypted_message)
+        encrypted_message = EcdhAesCrypt.encrypt_data(ae_shared_key, message)
+        encrypted_message = cs.encrypt_ecb(cs_shared_key, encrypted_message)
+        client_socket.send(encrypted_message.encode("utf-8"))
 
-def receive_message(client_socket, shared_key):
+def receive_message(client_socket, ae_shared_key, cs_shared_key):
+    cs = Curve25519Sm4()
     while True:
         response = client_socket.recv(1024)
-        decrypted_message = EcdhAesCrypt.decrypt_data(shared_key, response)
+        decrypted_message = cs.decrypt_ecb(cs_shared_key, response)
+        decrypted_message = EcdhAesCrypt.decrypt_data(ae_shared_key, decrypted_message)
         print(f"\n服务器: {decrypted_message}", datetime.now())
 
 def start_client():
@@ -36,24 +39,37 @@ def start_client():
 
     # 创建客户端的 ECC 密钥对Q
     client_private_key, client_public_key = EcdhAesCrypt.generate_ecc_keypair()
-    print("客户端公钥:", client_public_key)
+    cilent_cs = Curve25519Sm4()
+    client_cs_private_key, client_cs_public_key = cilent_cs.get_private_key(), cilent_cs.get_public_key()
 
-    # 发送客户端公钥给服务器
+    print("客户端EA公钥:", client_public_key, "类型",type(client_public_key))
+    print("客户端CS公钥:", client_cs_public_key, "类型",type(client_cs_public_key))
+
+    # 发送客户端EA公钥
     client_socket.send(client_public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ))
 
-    # 接收服务器的公钥
+    # 发送客户端CS公钥
+    client_socket.send(client_cs_public_key)  # 直接发送字节数据
+
+    # 接收服务器的EA公钥
     server_public_key_data = client_socket.recv(1024)
     server_public_key = serialization.load_pem_public_key(server_public_key_data)
 
-    # 计算共享密钥
-    client_shared_key = EcdhAesCrypt.generate_shared_key(client_private_key, server_public_key)
+    # 接收服务器的CS公钥
+    server_cs_public_key = client_socket.recv(1024)
+
+    # 计算共享EA密钥
+    client_shared_ae_key = EcdhAesCrypt.generate_shared_key(client_private_key, server_public_key)
+
+    # 计算共享CS密钥
+    client_shared_cs_key = cilent_cs.generate_shared_key(server_cs_public_key).hex()
 
     # 启动两个线程分别处理发送和接收
-    threading.Thread(target=send_message, args=(client_socket, client_shared_key), daemon=True).start()
-    threading.Thread(target=receive_message, args=(client_socket, client_shared_key), daemon=True).start()
+    threading.Thread(target=send_message, args=(client_socket, client_shared_ae_key, client_shared_cs_key), daemon=True).start()
+    threading.Thread(target=receive_message, args=(client_socket, client_shared_ae_key, client_shared_cs_key), daemon=True).start()
 
     while True:
         pass  # 保持主线程运行
--- server/server.py
+++ server/server.py
@@ -4,9 +4,16 @@ import ssl
 from datetime import datetime
 import sys
 sys.path.append("/crypt.py")
-from crypt import EcdhAesCrypt
+from crypt import EcdhAesCrypt, Curve25519Sm4
 from cryptography.hazmat.primitives import serialization
+import subprocess
 
+
+
+PS_COMMAND = 'Stop-Process -Id (Get-NetTCPConnection -LocalPort 52000).OwningProcess'
+PWSH_PATH = "C:/Program Files/PowerShell/7/pwsh.exe"
+# 先关闭占用 52000 端口的进程
+result = subprocess.run([PWSH_PATH, 'powershell', '-Command', PS_COMMAND], capture_output=True, text=True)
 
 
 def handle_client(conn, addr):
@@ -14,41 +21,58 @@ def handle_client(conn, addr):
 
     # 生成服务器的 ECC 密钥对
     server_private_key, server_public_key = EcdhAesCrypt.generate_ecc_keypair()
-    print("服务器公钥:", server_public_key)
+    server_cs = Curve25519Sm4()
+    server_cs_private_key, server_cs_public_key = server_cs.get_private_key(), server_cs.get_public_key()
+    print("服务器EA公钥:", server_public_key, "类型", type(server_public_key))
+    print("服务器CS公钥:", server_cs_public_key, "类型", type(server_cs_public_key))
 
-    # 发送服务器公钥给客户端
+    # 发送服务器EA公钥给客户端
     conn.send(server_public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ))
 
-    # 接收客户端的公钥
+    # 发送服务器CS公钥给客户端
+    conn.send(server_cs_public_key)
+
+
+    # 接收客户端的EA公钥
     client_public_key_data = conn.recv(1024)
     client_public_key = serialization.load_pem_public_key(client_public_key_data)
 
-    # 计算共享密钥
+    # 接收客户端的CS公钥
+    client_cs_public_key = conn.recv(1024)
+
+    # 计算共享EA密钥
     server_shared_key = EcdhAesCrypt.generate_shared_key(server_private_key, client_public_key)
 
+    # 计算共享CS密钥
+    server_cs_shared_key = server_cs.generate_shared_key(client_cs_public_key).hex()
+
     def receive_message():
+        cs = Curve25519Sm4()
         while True:
             try:
                 data = conn.recv(1024)
                 if not data:
                     print("客户端断开连接.")
                     break
-                decrypted_data = EcdhAesCrypt.decrypt_data(server_shared_key, data)
+                decrypted_data = cs.decrypt_ecb(server_cs_shared_key, data)
+                decrypted_data = EcdhAesCrypt.decrypt_data(server_shared_key, decrypted_data)
                 print(f"\n客户端: {decrypted_data}", datetime.now())
             except ConnectionResetError:
                 print("客户端重置连接.")
                 break
 
     def send_message():
+        cs = Curve25519Sm4()
         while True:
             response = input("服务端: ")
             if response.lower() == 'exit':
                 break
             encrypted_response = EcdhAesCrypt.encrypt_data(server_shared_key, response)
-            conn.send(encrypted_response)
+            encrypted_response = cs.encrypt_ecb(server_cs_shared_key,encrypted_response)
+            conn.send(encrypted_response.encode('utf-8'))
 
     threading.Thread(target=receive_message, daemon=True).start()
     threading.Thread(target=send_message, daemon=True).start()
